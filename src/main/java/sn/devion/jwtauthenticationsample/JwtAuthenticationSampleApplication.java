239d5dafda2aa778a5026908155175f97f512aea
filipcisco <Ndiogoye.Cisco@outlook.com>
2023-09-10 18:35:08 +0000

    Initialize repo

---
+++ src/main/java/sn/devion/jwtauthenticationsample/JwtAuthenticationSampleApplication.java
@@ -0,0 +1,13 @@
+package sn.devion.jwtauthenticationsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtAuthenticationSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JwtAuthenticationSampleApplication.class, args);
+    }
+
+}
